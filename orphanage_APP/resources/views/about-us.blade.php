2170adb6379a220c954ecf937ea3311d4aacdb66
0~KB <makyararich@gmail.com>
2021-01-27 17:08:52 +0300

    HomePageFeatureAdded
    About page, Home Page, Login Page

---
+++ orphanage_APP/resources/views/about-us.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+
+<h1>This is about our page</h1>
+<ul type=">>">
+  <li>The main objective of this project is to develop a web application for the orphanage to analyze the current requirement and to schedule the donors.</li>
+  <li>The high standard of living has resulted in the wastage of food, clothes, etc.</li>
+  <li>A donor can view the list of items put up by seekers and can donate the same, if possible.</li>
+  <li>In the same way, seekers can view the list of items put up by donors and if required, can claim the donated item by contacting the donor</li>
+  <li>The proposed application include a reduction in wastage of food, making food, making food, clothes, etc</li>
+</ul>  
+
+
+
+
+
+@endsection
+
---
+++ orphanage_APP/resources/views/service.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+
+<h1>this is about our services</h1>
+<ol type="1">
+<li>Food: Food and clean water are the most basic need for all children. Food supplied from external sources could actually reduce food security in the long run if it disrupts the local market, but food procured locally or produced through community gardens can contribute to local food security. Food may be provided as either bulk grain needing preparation or as cooked meals.    </li>
+  <li>Health care: The need for health care includes childhood immunizations and vitamin supplements for children under five, routine health care for all and reproductive health services for older children aged 10–17. In some countries health care is free for all children or for the youngest children. However, patients often have to pay for drugs and supplies. Some have argued that orphan-related programmes should advocate for free health care for all children rather than focus on providing funding to pay for care for children. However, in that case additional resources would need to be made available to governments to provide free care to families.</li> 
+  <li>Community support: This includes identification of vulnerable children and funding for community workers who can assess needs, organize support and provide some counselling and individual support. Many community workers will be volunteers but significant funds may still be required for training and transportation.</li>
+  <li>Education: This includes school fees where they exist, funds required for uniforms, books and other supplies, and special fees. Many countries have eliminated school fees and additional advocacy efforts could help to eliminate them in other countries as well, but the extra costs of uniforms, supplies and special assessments can still be substantial.</li>
+  <li>Family/home support: This category includes clothes, shoes, bednets and economic self-sufficiency. The need for bednets will vary depending on local climate and other conditions. In many cases donated clothes and shoes are available at no cost, but reliance on donated goods may not be sustainable as programmes scale up considerably. Economic self-sufficiency refers to programmes to provide older children and/or their families with economic support such as microfinance loans, skills training, grants or seeds.</li>
+</ol>  
+
+
+
+@endsection

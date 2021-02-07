e912bcee9059b0e6fc0f36d8f8eddb7d36b19cb6
Gabby1025 <gabbymturi2@gmail.com>
2021-02-07 10:26:53 +0300

    linking two pages

--- orphanage_APP/app/Donor123.php
+++ orphanage_APP/app/Donor123.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Donor123 extends Model
+class StudInsert extends Model
 {
-    //
+    protected $table = 'donations_details';
+    public $timestamps = true;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'last_name', 'amount', 'email',
+    ];
 }

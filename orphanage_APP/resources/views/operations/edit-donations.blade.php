4f0796a32e6697287a84655aeb3cb9ccb7232d85
Gabby1024 <gabbymturi2@gmail.com>
2021-02-08 21:01:04 +0300

    .

--- orphanage_APP/resources/views/operations/edit-donations.blade.php
+++ orphanage_APP/resources/views/operations/edit-donations.blade.php
@@ -14,18 +14,6 @@
                         <label>Last Name</label>
                         <input type="text" class="form-control">
                       </div>
-                     <div class="form-group">
-                        <label>Choose Amount</label>
-                        <select class="form-control">
-                          <option>Tsh. 25,000</option>
-                          <option>Tsh. 50,000</option>
-                          <option>Tsh. 100,000</option>
-                          <option>Tsh. 250,000</option>
-                          <option>Tsh. 500,000<option>
-                              <option>Tsh. 1,000,000</option>
-                              <option>Not In List</option>
-                        </select>
-                     </div>
                     <div class="form-group">
                         <label>Enter Amount</label>
                         <input type="text" class="form-control">

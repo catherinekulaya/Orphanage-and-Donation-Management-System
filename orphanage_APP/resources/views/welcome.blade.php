81e2c75d820351a22cb1941f0dec9ccdb062f301
Gabby1024 <gabbymturi2@gmail.com>
2021-02-08 22:16:19 +0300

    Update welcome.blade.php

--- orphanage_APP/resources/views/welcome.blade.php
+++ orphanage_APP/resources/views/welcome.blade.php
@@ -166,12 +166,7 @@ color: white;" href="{{url('/service')}}">SERVICE</a>
             </div>
             <div class="links">
 
-                    <!-- <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
+                 
             </div>
 
 

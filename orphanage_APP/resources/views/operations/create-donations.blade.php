ca6a21c7b5d83aeb0ca7d86ac83883df803f72c1
Gabby1024 <gabbymturi2@gmail.com>
2021-02-08 20:55:15 +0300

    deleting some codes for displaying date

--- orphanage_APP/resources/views/operations/create-donations.blade.php
+++ orphanage_APP/resources/views/operations/create-donations.blade.php
@@ -33,13 +33,6 @@
                         <label>Email</label>
                         <input type="text" class="form-control">
                     </div>
-                    <div class="form-group">
-                        <label>Incurred on</label>
-                        <input type="date" class="form-control">
-                    </div>
-
-
-
                     <button type="submit" class="btn btn-primary">Submit</button>
                   </form>
             </div>

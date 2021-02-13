2c6131df661538b2cc256139e64918cbff264f6f
Eliusmduda <51673099+Eliusmduda@users.noreply.github.com>
2021-02-14 02:08:46 +0300

    Modified donation type selection

--- orphanage_APP/resources/views/user/donate.blade.php
+++ orphanage_APP/resources/views/user/donate.blade.php
@@ -21,11 +21,11 @@
                               <option value="Item">Item</option>
                             </select>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group" id="item_name">
                           <label>Item Name</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" value="">
                         </div>
-                        <div class="form-group">
+                        <div class="form-group" id="amount">
                             <label>Amount</label>
                             <input type="text" class="form-control">
                         </div>
@@ -41,6 +41,26 @@
                             <a href="/"  class="btn btn-primary">Cancel</a>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
+
+                        <script>
+                        $('document').ready(function () {
+                        $("#donationType").change(function () {
+                        var data = $(this).val();
+                        if (data == "Money") {
+                        $('#item_name').hide();
+                        } else {
+                        $('#item_name').show();
+                        }
+
+                        if (data == "Item") {
+                        $('#amount').hide();
+                        } else {
+                        $('#amount').show();
+                        }
+
+                        });
+                        });
+                        </script>
                       </form>
                 </div>
               </div>

1888ddb4a716f24036fae8ae8eb715e5bc9185e6
Shemm Junior <juniorshemm@gmail.com>
2021-02-16 22:00:47 +0300

    Added Items Required Database link in itemsRequired related files

--- orphanage_APP/resources/views/itemsrequested.blade.php
+++ orphanage_APP/resources/views/itemsrequested.blade.php
@@ -2,40 +2,74 @@
 
 @section('content')
 
-<div class="home-container table-responsive-sm">
-    <div class="row">
-        <div class="col-sm-10">
-    <h4>Items requested</h4>
-        </div>
-        <div class="col-sm-2">
-            <a class="btn btn-primary btn-sm" href="/itemsrequested/create">Add Item</a>
-        </div>
-    </div>
-    <br>
-<table class="table table-hover table-bordered table-responsive-sm">
-    <thead>
-        <tr>
-            <th scope="col">No</th>
-            <th scope="col">Items requested</th>
-            <th scope="col">Orphanage center</th>
-            <th scope="col">Options</th>
+    <div class="home-container table-responsive-sm">
+        <div class="row">
+            <div class="col-sm-10">
+                <h4>Items requested</h4>
+            </div>
+            <div class="col-sm-2">
+                <a class="btn btn-primary btn-sm" href="/itemsrequested/create">Add Item</a>
 
-        </tr>
-        <thead>
-            <tbody>
-                @foreach($itemsrequested as $itemrequest)
+
+            </div>
+        </div>
+        @if (session('response'))
+            <div class="row">
+                <div class="alert alert-success col-sm-10" id="success-alert" role="alert">
+                    {{ session('response') }}
+                </div>
+            </div>
+        @endif
+
+        <script>
+            $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+                $("#success-alert").slideUp(500);
+            });
+
+            $(document).ready(function() {
+
+                $("#showToast").click(function() {
+                    $('.toast').toast('show');
+                });
+
+            });
+
+        </script>
+
+        <br>
+        <table class="table table-hover table-bordered">
+            <thead>
                 <tr>
-                    <th scope="row"> {{$loop->index+1 }}</th>
-                    <td> {{$itemrequest['itemsrequested']}}</td>
-                    <td> {{$itemrequest['orphanagecenter']}}</td>
-                    <td>
-                <a class="btn btn-primary btn-sm" href="/itemsrequested/edit/77">Edit</a>
-           <a class="btn btn-danger btn-sm">Delete</a>
-            </td>
-          </tr>
-                @endforeach
-            </tbody>
+                    <th scope="col">No</th>
+                    <th scope="col">Items Requested</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Date Requested</th>
+                    <th scope="col">Options</th>
+                </tr>
+                <thead>
+                <tbody>
+                    @foreach ($items as $item)
+                        <tr>
+                            <th scope="row"> {{ $loop->index + 1 }}</th>
+                            <td> {{ $item['itemName'] }}</td>
+                            <td> {{ $item['quantity'] }}</td>
+                            <td> {{ $item['date'] }}</td>
+                            <td>
+                                <div class="row">
+                                    <a class="btn btn-primary btn-sm"
+                                        href="/itemsrequested/edit/{{ $item->id }}">Edit</a> &nbsp;
+                                    <form class="form-size" action="/itemsrequested/delete/{{ $item->id }}"
+                                        method="POST">
+                                        <button class="btn btn-danger btn-sm">Delete</button>
+                                        @csrf
+                                        @method('DELETE')
+                                    </form>
+                                </div>
+
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
         </table>
     </div>
-    @endsection
-
+@endsection
--- orphanage_APP/resources/views/layouts/app.blade.php
+++ orphanage_APP/resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/moment.js') }}"></script>
     <script src="{{ asset('js/chart.js') }}"></script>
-
+    <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
 
 
     <!-- Fonts -->

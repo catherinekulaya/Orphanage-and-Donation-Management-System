0918675f6a56f9d7755b1abebf273ac0f07e6d69
0~KB <makyararich@gmail.com>
2021-02-17 12:12:20 +0300

    Update welcome.blade.php

--- orphanage_APP/resources/views/welcome.blade.php
+++ orphanage_APP/resources/views/welcome.blade.php
@@ -84,10 +84,12 @@ padding-top: 18px;
 position: fixed;
 top: 0px;">
 
+
             @if (Route::has('login'))
                     @auth
                         <a style="
 color: white;" href="{{ url('/home') }}">Home</a>
+
                     @else
                         <a style="
 color: white;"  href="{{ route('login') }}">Login</a>
@@ -95,16 +97,22 @@ color: white;"  href="{{ route('login') }}">Login</a>
                         @if (Route::has('register'))
                             <a style="
 color: white;"  href="{{ route('register') }}">Register</a>
+
                         @endif
                     @endauth
                
             @endif
             <a style="
 color: white;"  href="{{ url('/about-us') }}">ABOUT-US</a>
+
                     <a style="
 color: white;" href="{{url('/service')}}">SERVICE</a>
+ 
             </div>
             <div class="links">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+            
                    
                     <!-- <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>

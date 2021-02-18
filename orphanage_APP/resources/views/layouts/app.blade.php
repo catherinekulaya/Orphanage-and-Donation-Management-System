afea87b4297a7ef901a34e2bba81a4880a70c0e1
catherinekulaya <catherinekulaya97@gmail.com>
2021-02-18 06:54:38 +0300

    Removed Sidenav stylings

--- orphanage_APP/resources/views/layouts/app.blade.php
+++ orphanage_APP/resources/views/layouts/app.blade.php
@@ -28,114 +28,91 @@
 <body>
 
     <div id="app">
-      <nav class="mnb navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <i class="ic fa fa-bars"></i>
-            </button>
-            <div style="padding: 15px 0;">
-               <a href="#" id="msbo"><i class="ic fa fa-bars"></i></a>
-            </div>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">En</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Draude Oba <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Settings</a></li>
-                  <li><a href="#">Upgrade</a></li>
-                  <li><a href="#">Help</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Logout</a></li>
-                </ul>
-              </li>
-              <li><a href="#"><i class="fa fa-bell-o"></i></a></li>
-              <li><a href="#"><i class="fa fa-comment-o"></i></a></li>
-            </ul>
-            <form class="navbar-form navbar-right">
-              <input type="text" class="form-control" placeholder="Search...">
-            </form>
-          </div>
-        </div>
-      </nav>
-      <!--msb: main sidebar-->
-      <div class="msb" id="msb">
-          <nav class="navbar navbar-default" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <div class="brand-wrapper">
-                <!-- Brand -->
-                <div class="brand-name-wrapper">
-                  <a class="navbar-brand" href="#">
-                    SAITAMA
-                  </a>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                   Orphanage Donation Management
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        {{-- TODO: Remove Logi and Register in The Navbar --}}
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/home">Dashboard <span class="sr-only">(current)</span></a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="/view_donations">Donations</a>
+
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Schedule</a>
+                          </li>
+                         
+                          <li class="nav-item">
+                            <a class="nav-link" href="/orphans">Orphans</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="/itemsrequested">Items Requested</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="/expenses">Expenses</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="/payments">Payments</a>
+                          </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                  <img src={{ url('/images/user.png') }}>
+                                    {{ Auth::user()->name }}
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
                 </div>
-      
-              </div>
-      
             </div>
-      
-            <!-- Main Menu -->
-            <div class="side-menu-container">
-              <ul class="nav navbar-nav">
-      
-                <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-                <li class="active"><a href="#"><i class="fa fa-puzzle-piece"></i> Components</a></li>
-                <li><a href="#"><i class="fa fa-heart"></i> Extras</a></li>
-      
-                <!-- Dropdown-->
-                <li class="panel panel-default" id="dropdown">
-                  <a data-toggle="collapse" href="#dropdown-lvl1">
-                    <i class="fa fa-diamond"></i> Apps
-                    <span class="caret"></span>
-                  </a>
-                  <!-- Dropdown level 1 -->
-                  <div id="dropdown-lvl1" class="panel-collapse collapse">
-                    <div class="panel-body">
-                      <ul class="nav navbar-nav">
-                        <li><a href="#">Mail</a></li>
-                        <li><a href="#">Calendar</a></li>
-                        <li><a href="#">Ecommerce</a></li>
-                        <li><a href="#">User</a></li>
-                        <li><a href="#">Social</a></li>
-      
-                        <!-- Dropdown level 2 -->
-                        <li class="panel panel-default" id="dropdown">
-                          <a data-toggle="collapse" href="#dropdown-lvl2">
-                            <i class="glyphicon glyphicon-off"></i> Sub Level <span class="caret"></span>
-                          </a>
-                          <div id="dropdown-lvl2" class="panel-collapse collapse">
-                            <div class="panel-body">
-                              <ul class="nav navbar-nav">
-                                <li><a href="#">Link</a></li>
-                                <li><a href="#">Link</a></li>
-                                <li><a href="#">Link</a></li>
-                              </ul>
-                            </div>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </li>
-                <li><a href="#"><span class="glyphicon glyphicon-signal"></span> Link</a></li>
-              </ul>
-            </div><!-- /.navbar-collapse -->
-          </nav>  
+        </nav>
         <main class="py-4">
             @yield('content')
         </main>
     </div>
-    <script>
-      (function(){
-      $('#msbo').on('click', function(){
-      $('body').toggleClass('msb-x');
-       });
-      }());
-    </script>
-
+ {{-- <script src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+ <script>
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  </script> --}}
 </body>
 </html>

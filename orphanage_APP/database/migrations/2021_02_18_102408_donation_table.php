47e98dfb6d7d036a823f665c3d491667a07b9f27
Shemm Junior <juniorshemm@gmail.com>
2021-02-18 13:40:15 +0300

    Populated Donations Graph in dashboard

--- orphanage_APP/database/migrations/2021_02_18_102408_donation_table.php
+++ orphanage_APP/database/migrations/2021_02_18_102408_donation_table.php
@@ -4,8 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Donate extends Migration
+class DonationTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('donate', function (Blueprint $table) {
@@ -14,7 +19,7 @@ class Donate extends Migration
             $table->string('item')->nullable();
             $table->string('money')->nullable();
             $table->text('description')->nullable();
-            $table->string('donationDate');
+            $table->date('donationDate');
             $table->timestamps();
         });
     }

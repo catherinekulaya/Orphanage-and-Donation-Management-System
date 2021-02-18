9afb64e43fa8a8c1503ca2fdd3e555ac4055dafa
catherinekulaya <catherinekulaya97@gmail.com>
2021-02-18 07:16:08 +0300

    Fixed Admin Login and Dashboard topbar

--- orphanage_APP/resources/views/layouts/app.blade.php
+++ orphanage_APP/resources/views/layouts/app.blade.php
@@ -31,9 +31,23 @@
       <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">ODMS</a>
         <ul class="navbar-nav px-3">
-          <li class="nav-item text-nowrap">
-            <a class="nav-link" href="#">Sign out</a>
-          </li>
+          <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              <img src={{ url('/images/user.png') }}>
+                {{ Auth::user()->name }}
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+        </li>
         </ul>
       </nav>
   

a47e23631909d0d9074b7a21f3a1f8e10ae01f3d
catherinekulaya <catherinekulaya97@gmail.com>
2021-02-18 06:50:25 +0300

    Added sidenav styling in resources/sass/app.scss

--- orphanage_APP/public/js/app.js
+++ orphanage_APP/public/js/app.js
@@ -49900,8 +49900,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\orphanage_APP\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\orphanage_APP\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Demo\Documents\GitHub\Orphanage-and-Donation-Management-System\orphanage_APP\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Demo\Documents\GitHub\Orphanage-and-Donation-Management-System\orphanage_APP\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

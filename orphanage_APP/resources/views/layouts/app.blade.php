c14163c3aca6a436afc94eb61b293b15fc106813
catherinekulaya <catherinekulaya97@gmail.com>
2021-02-18 07:23:53 +0300

    Update app.blade.php

--- orphanage_APP/resources/views/layouts/app.blade.php
+++ orphanage_APP/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,89 +26,94 @@
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 
 </head>
+
 <body>
 
     <div id="app">
-      <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">ODMS</a>
-        <ul class="navbar-nav px-3">
-          <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-              <img src={{ url('/images/user.png') }}>
-                {{ Auth::user()->name }}
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
+        <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+            <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">ODMS</a>
+            <ul class="navbar-nav px-3">
+                <li>
+                    <a class="nav-link" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+                </li>
+            </ul>
+        </nav>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                    <div class="sidebar-sticky">
+                        <h6
+                            class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                            <span>MANAGEMENT</span>
+                            <a class="d-flex align-items-center text-muted" href="#">
+                                <span data-feather="plus-circle"></span>
+                            </a>
+                        </h6>
+                        <ul class="nav flex-column">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/home">Dashboard <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/view_donations">Donations</a>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Schedule</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="/orphans">Orphans</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/itemsrequested">Items Requested</a>
+                            </li>
+
+                        </ul>
+
+                        <h6
+                            class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                            <span>Finance</span>
+                            <a class="d-flex align-items-center text-muted" href="#">
+                                <span data-feather="plus-circle"></span>
+                            </a>
+                        </h6>
+                        <ul class="nav flex-column mb-2">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/expenses">Expenses</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/payments">Payments</a>
+                            </li>
+
+                            <li>
+                              <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                                  {{ __('Logout') }}
+                              </a>
+  
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                  @csrf
+                              </form>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </nav>
+
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                    <main class="py-4">
+                        @yield('content')
+                    </main>
+
+                </main>
             </div>
-        </li>
-        </ul>
-      </nav>
-  
-      <div class="container-fluid">
-        <div class="row">
-          <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-            <div class="sidebar-sticky">
-              <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <span>MANAGEMENT</span>
-                <a class="d-flex align-items-center text-muted" href="#">
-                  <span data-feather="plus-circle"></span>
-                </a>
-              </h6>
-              <ul class="nav flex-column">
-                <li class="nav-item active">
-                  <a class="nav-link" href="/home">Dashboard <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/view_donations">Donations</a>
-
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Schedule</a>
-                </li>
-               
-                <li class="nav-item">
-                  <a class="nav-link" href="/orphans">Orphans</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/itemsrequested">Items Requested</a>
-                </li>
-              
-              </ul>
-  
-              <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                <span>Finance</span>
-                <a class="d-flex align-items-center text-muted" href="#">
-                  <span data-feather="plus-circle"></span>
-                </a>
-              </h6>
-              <ul class="nav flex-column mb-2">
-                <li class="nav-item">
-                  <a class="nav-link" href="/expenses">Expenses</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/payments">Payments</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-  
-          <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-            <main class="py-4">
-              @yield('content')
-          </main>
-
-          </main>
         </div>
-      </div>
-  
+
     </div>
 </body>
+
 </html>
